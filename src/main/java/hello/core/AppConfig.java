33c4753497130f1219296cfd5bd279042bb1033d
hyechan <dhgpcks@gmail.com>
2021-04-16 11:37:42 +0900

    java -> spring, IoC/DI/(DI)(스프링)컨테이너

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,23 +8,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //AppConfig는 애플리케이션의 실제 동작에 필요한 "구현 객체를 생성"한다.
 //AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결) 해준다.
+@Configuration
 public class AppConfig {
 
+    @Bean
+    //Bean을 적으면 다 Spring 컨테이너란 곳에 등록 될 애들이다!
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
