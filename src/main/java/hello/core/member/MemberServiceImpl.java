e7811eb8e99f9554ae93f6b71930e7946ad6dc4c
hyechan <dhgpcks@gmail.com>
2021-04-21 15:19:49 +0900

    @컴포넌트, @컴포넌트스캔

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -7,7 +7,15 @@ import org.springframework.stereotype.Component;
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
 
-    @Autowired //ac.getBean(MemberRepository.class)
+    /*
+    자, 처음에 만들때는 AppConfig 객체생성해서, appConfig.memberRepository()로 가져왔어.
+    그러면 MemoryMemberRepository()객체를 생성해서 반환해주는 방식을 썼어.
+    이 부분을 스프링은 bean으로 관리를 하게 돼. bean으로 Annotation~에서 bean꺼내서 할당해줬어.
+    ac.getBean(MemberRepository.class)
+
+     */
+
+    @Autowired //ac.getBean(MemberRepository.class) // MemberRepository에 맞는 구체화 가지고 와서 설정해줌.
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
