7bb9db7530d4413156453c1bae5b7398c0809846
hyechan <dhgpcks@gmail.com>
2021-05-01 20:07:05 +0900

    ObjectProvider, JSR-330 Provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,7 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -36,21 +37,28 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; //생성시점에 주입
+//        private final PrototypeBean prototypeBean; //생성시점에 주입
+
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeansProvider;
+//        private Provider<PrototypeBean> prototypeBeansProvider;
+
 
 //        @Autowired
 //        ApplicationContext applicationContext; //무식하게 그냥 받아버리는 방법도 있긴 함. -> 해결방법이 뭘까?
-        @Autowired
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean){
+//            this.prototypeBean = prototypeBean;
+//        }
         public int logic(){
 //            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+//            PrototypeBean prototypeBean = prototypeBeansProvider.get(); // 자바표준 provider
+            PrototypeBean prototypeBean = prototypeBeansProvider.getObject(); //스프링 꺼 ObjectProvider
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
